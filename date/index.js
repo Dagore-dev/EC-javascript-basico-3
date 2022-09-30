5ce556f654fee76d1a3c2da3c5562c0b4251232f
Dagore-dev <david.gomez.redondo.dev@gmail.com>
2022-09-30 12:45:55 +0200

    Date

---
+++ date/index.js
@@ -0,0 +1,8 @@
+const time = document.getElementById('time')
+
+const date = new Date()
+const currentYear = date.getFullYear()
+const currentDate = date.getTime()
+const lastDayOfYear = Date.parse(`01/01/${currentYear + 1}`)
+
+time.textContent = lastDayOfYear - currentDate
