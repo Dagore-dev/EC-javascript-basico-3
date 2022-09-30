ba109e243229d6e65128622bc0d765041888c281
Dagore-dev <david.gomez.redondo.dev@gmail.com>
2022-09-30 13:26:08 +0200

    date

--- date/index.js
+++ date/index.js
@@ -1,8 +1,11 @@
-const time = document.getElementById('time')
+window.setInterval(millisecondsUntilEndOfYear, 1)
 
-const date = new Date()
-const currentYear = date.getFullYear()
-const currentDate = date.getTime()
-const lastDayOfYear = Date.parse(`01/01/${currentYear + 1}`)
+function millisecondsUntilEndOfYear () {
+  const time = document.getElementById('time')
+  const date = new Date()
+  const currentYear = date.getFullYear()
+  const currentDate = date.getTime()
+  const lastDayOfYear = Date.parse(`01/01/${currentYear + 1}`)
 
-time.textContent = lastDayOfYear - currentDate
+  time.textContent = lastDayOfYear - currentDate
+}
